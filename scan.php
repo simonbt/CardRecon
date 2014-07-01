bea1dfc59d0c2835ec2397382cc154424b93ef10
Simon Beattie <mail@simonbeattie.net>
2014-07-01 13:27:57 +0100

    initial commit

---
+++ scan.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * StormRecon - scan.php.
+ * User: simonbeattie
+ * Date: 01/07/2014
+ * Time: 13:25
+ */
+
+include_once('Library/bootstrap.php');
+
+$scanner = new \Library\AgentControl('1');
+
